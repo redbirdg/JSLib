d320dfd75503323e06be427377d609b624266186
redbirdg <129257704+redbirdg@users.noreply.github.com>
2024-08-03 17:58:17 +0800

    Delay, Find

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
+class Common
+{
+    /**
+     * await Delay(millisec);
+     * @param {number} millisec
+     */
+    static Delay = (millisec) => new Promise(resolve => setTimeout(() => resolve(), millisec));
+}
 
+class Doc
+{
+    /**
+     * Returns the first element that is a descendant of node that matches selectors.
+     * @param {number} retry default: -1, retry times, <0 means infinite
+     * @param {number} delay default: 1000, delay millisec to retry
+     */
+    static async Find(selectors, retry = -1, delay = 1000)
+    {
+        let curRetryCnt = 0;
+        while (true)
+        {
+            const node = document.querySelector(selectors);
+            if (node)
+                return node;
+            if (retry >= 0 && ++curRetryCnt > retry)
+                return null;
+            await Common.Delay(delay);
+            console.log(`Not found '${selectors}', delay ${delay} ms to retry`);
+        }
+    }
+}
